b00304a42385fcc4e33a9bd6d33246d40a006a50
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-04 00:02:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import {
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
-import Features from "@/components/Featured;
+import Features from "@/components/Features";
 import CTA from "@/components/Cta";
 
   
