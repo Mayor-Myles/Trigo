9c98513995aac9ed5fbaee4fb29c7003538b88bd
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-03 22:04:18 +0100

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,14 +2,18 @@
 
 
 // components/Hero.jsx
-import { Box, Heading, Text, Button, Stack, Icon } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, Stack, Icon,useColorMode } from "@chakra-ui/react";
 import Link from "next/link";
 import { GoPackage } from "react-icons/go";
 import { CiDeliveryTruck } from "react-icons/ci";
 
 const Hero = () => {
+
+  const {colorMode} = useColorMode();
+  
+  
   return (
-    <Box textAlign="center" py={20} px={6} bg="gray.50">
+    <Box textAlign="center" py={20} px={6} bg={colorMode="light" && "gray.50"}>
       <Heading size="2xl" mb={4}>
         Fast & Reliable Tricycle Deliveries
       </Heading>
@@ -26,11 +30,11 @@ const Hero = () => {
         mt={6}
       >
         <Button as={Link} href="/login?&role=business" colorScheme="blue" size="lg">
-         <Icon name={<GoPackage />} mx={4} size="md" /> Send a Package
+         <GoPackage mx={4} size="md" /> Send a Package
         </Button>
         
-        <Button href="/login?&role=rider" as={Link} colorScheme="black" variant="solid" size="lg">
-          <Icon name={<CiDeliveryTruck/>} mx={4} size="md" /> Become a Rider
+        <Button href="/login?&role=rider" as={Link} color="white" bg="black" variant="solid" size="lg">
+          <CiDeliveryTruck mx={4} size="md" /> Become a Rider
         </Button>
       </Stack>
     </Box>
