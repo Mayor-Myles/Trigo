5611241d570de0ff468353b23d8a35abefb5f157
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-04 00:02:08 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Features from "@/components/Featured;
-import CTA from "@/components/Cta;
+import CTA from "@/components/Cta";
 
   
 export default function App(){
