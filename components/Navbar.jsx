4bcf66ca44a4231dd827f377aeb599306274122e
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-04 11:18:50 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 // components/Navbar.jsx
 import { Flex, Text, Button, HStack, IconButton,useColorMode } from "@chakra-ui/react";
 import { CiLight, CiDark } from "react-icons/ci";
-
+  import Link from "next/link";
 
 const Navbar = () => {
 
@@ -13,7 +13,7 @@ const Navbar = () => {
       py={4}
       align="center"
       justify="space-between"
-      bg={colorMode=="light" && "white"}
+      bg={colorMode=="light" ? "white" : "black"}
       boxShadow="sm"
       position="sticky"
       top="0"
@@ -30,8 +30,8 @@ const Navbar = () => {
       icon={colorMode=="light" ? <CiDark fontSize="25px" /> : <CiLight fontSize="25px" /> }
       aria-label="Toggle Color Mode"
     />
-        <Button variant="ghost">Login</Button>
-        <Button colorScheme="blue">Get Started</Button>
+        <Button as={Link} href="/login?role=admin" variant="ghost">Login</Button>
+        <Button as={Link} href="/register" colorScheme="blue">Get Started</Button>
       </HStack>
 
       
