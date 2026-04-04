2653f3fad3237a87bf09a87e1c68699c15f16c71
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-04 13:25:35 +0100

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,11 +4,24 @@ import { Box, Heading, Text, Button, Stack, Icon,useColorMode } from "@chakra-ui
 import Link from "next/link";
 import { GoPackage } from "react-icons/go";
 import { CiDeliveryTruck } from "react-icons/ci";
+import {useState} from "react";
+
 
 const Hero = () => {
 
   const {colorMode,toggleColorMode} = useColorMode();
-  
+
+  const [loading,setLoading1] = useState(false);
+  const [loading,setLoading2] = useState(false);
+  const setters = [setLoading1,setloading2];
+
+const load = (type) => {
+
+const setLoading = setters[type];
+
+  setTimeout(setLoading(false),500);
+
+}
   
   return (
     <Box textAlign="center" py={10} px={6} bg={colorMode=="light" && "gray.50"}>
@@ -27,11 +40,11 @@ const Hero = () => {
         justify="center"
         mt={6}
       >
-        <Button onClick={(e)=>{e.visibility ="none"}} leftIcon={<GoPackage size ="25px" />} as={Link} href="/login?&role=business" colorScheme="blue" size="lg">
+        <Button onClick={()=>load(0)} leftIcon={<GoPackage size ="25px" />} as={Link} href="/login?&role=business" colorScheme="blue" size="lg">
            Send a Package
         </Button>
         
-        <Button leftIcon={<CiDeliveryTruck size ="25px" />} href="/login?&role=rider" as={Link} color="white" bg="#36454F" variant="solid" size="lg">
+        <Button onClick={()=>load(1)} leftIcon={<CiDeliveryTruck size ="25px" />} href="/login?&role=rider" as={Link} color="white" bg="#36454F" variant="solid" size="lg"
            Become a Rider
         </Button>
       </Stack>
