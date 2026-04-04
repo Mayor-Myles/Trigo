811dbaeff6b6cd3ac9491876c29878950b2556de
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-04 11:14:32 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
-
+import ContactUs from "@/components/ContactUs";
   
 export default function App(){
 
