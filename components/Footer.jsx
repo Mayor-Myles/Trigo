55a66f80c637509b2ea3b20ec9cadf1ed0680e97
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-04 11:05:57 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box textAlign="center" py={6} collr="white" bg="black">
+    <Box textAlign="center" py={6} color="white" bg="black">
       <Text fontSize="sm">
         © {new Date().getFullYear()} Trigo. All rights reserved.
     | Mylezic  </Text>
