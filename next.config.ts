6313d27651049290b02cc3bef889e974ed8eb4bf
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-06 07:19:24 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+async rewrites() {
+    return [
+      {
+        // The "Mask" you use in React
+        source: '/api/backend/:path*', 
+        // The "Final Destination" where the data lives
+        destination: 'http://mylezic.myartsonline.com/:path*',
+      },
+    ]
+  },
+  
 };
 
 export default nextConfig;
