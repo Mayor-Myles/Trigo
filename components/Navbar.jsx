adb1396387664b7d103940c7be656ed6f5915d6e
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-08 09:17:47 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -46,9 +46,10 @@ setTimeout(()=>{setLoading(false)}, 1500);
       icon={colorMode=="light" ? <CiDark fontSize="25px" /> : <CiLight fontSize="25px" /> }
       aria-label="Toggle Color Mode"
     />
-        <Button display={isUser && "none"} onClick={()=>load(0)} isLoading={loading} as={Link} href="/login" variant="ghost">Login</Button>
-        <Button display={isUser && "none"} onClick={()=>load(1)} isLoading={loading2} as={Link} href="/register" colorScheme="blue">Get Started</Button>
-      </HStack>
+       {isUser && ( <Button onClick={()=>load(0)} isLoading={loading} as={Link} href="/login" variant="ghost">Login</Button>
+        <Button onClick={()=>load(1)} isLoading={loading2} as={Link} href="/register" colorScheme="blue">Get Started</Button>
+     )}
+    </HStack>
 
       
     </Flex>
