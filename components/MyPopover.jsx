1e8a3ecf33ed66d731dd7a9e1bd17c9598dc3c7c
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-10 07:40:25 +0100

    Create MyPopover.jsx

---
+++ components/MyPopover.jsx
@@ -0,0 +1,48 @@
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Box
+} from '@chakra-ui/react'
+
+
+export default function MyPopover({data}){
+
+
+  return(
+
+<Center>
+            <Popover isOpen={data.length > 0}>
+  
+  <PopoverContent>
+    <PopoverHeader>Results</PopoverHeader>
+    <PopoverCloseButton />
+    <PopoverBody>
+      
+      
+      {
+        data.map((item,i)=> (
+<Box my={2} key={i} boxShadow="sm">
+
+</Box>
+
+        ));
+
+      }
+      
+    </PopoverBody>
+  </PopoverContent>
+              </Popover>
+              </Center>
+
+
+);
+
+}
+  
