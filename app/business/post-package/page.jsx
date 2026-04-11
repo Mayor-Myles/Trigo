756d3f45748a44b52d189ba2cb2e23968c817d7f
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-11 13:57:43 +0100

    Update page.jsx

--- app/business/post-package/page.jsx
+++ app/business/post-package/page.jsx
@@ -105,7 +105,7 @@ toast({
     } else {
       //setPickupData([]);
       setPickupLoading(false);
-      console.log(pickupData);
+      
     }
   }, [debouncedPickupAddress]);
 
@@ -262,7 +262,7 @@ fetchUserData();
                   <Icon as={FiMapPin} color="green.400" />
                 </InputLeftElement>
                 <Input
-                value={pickupData?.properties.lon}
+                value={address}
                   onChange={(e)=>{setPickupAddress(e.target.value); setPickupLoading(true) }}
                   placeholder="e.g. 12 Broad Street, Lagos Island"
                   bg="gray.50" border="1.5px solid" borderColor="gray.200"
@@ -288,7 +288,7 @@ fetchUserData();
                   <Icon as={FiMapPin} color="red.400" />
                 </InputLeftElement>
                 <Input
-                  value={deliveryData?.properties.lon}
+                  value={address}
                   onChange={(e)=>{setDeliveryAddress(e.target.value); setDeliveryLoading(true)}}
                   placeholder="e.g. 5 Ikeja Avenue, Ikeja"
                   bg="gray.50" border="1.5px solid" borderColor="gray.200"
