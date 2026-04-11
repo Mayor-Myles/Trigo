17d487b5f66fcdd061c9c0f29a937d13971a814d
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-11 22:16:17 +0100

    Update jotai.js

--- utils/jotai.js
+++ utils/jotai.js
@@ -2,3 +2,9 @@
 import { atom } from "jotai";
 
 export const userDataAtom = atom(null);
+
+export const pickupDataAtom = atom(null);
+
+export const deliveryDataAtom = atom(null);
+export const isPickupAtom = atom("");
+export const isDeliveryAtom = atom("");
