c80945a567cfaefc49c9d223f975aaeb90296553
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-04-11 12:42:04 +0100

    Update page.jsx

--- app/business/post-package/page.jsx
+++ app/business/post-package/page.jsx
@@ -261,7 +261,7 @@ fetchUserData();
                   <Icon as={FiMapPin} color="green.400" />
                 </InputLeftElement>
                 <Input
-                value={pickupData.properties.lon}
+                value={pickupData?.properties.lon}
                   onChange={(e)=>{setPickupAddress(e.target.value); setPickupLoading(true) }}
                   placeholder="e.g. 12 Broad Street, Lagos Island"
                   bg="gray.50" border="1.5px solid" borderColor="gray.200"
@@ -287,7 +287,7 @@ fetchUserData();
                   <Icon as={FiMapPin} color="red.400" />
                 </InputLeftElement>
                 <Input
-                  value={deliveryData.properties.lon}
+                  value={deliveryData?.properties.lon}
                   onChange={(e)=>{setDeliveryAddress(e.target.value); setDeliveryLoading(true)}}
                   placeholder="e.g. 5 Ikeja Avenue, Ikeja"
                   bg="gray.50" border="1.5px solid" borderColor="gray.200"
